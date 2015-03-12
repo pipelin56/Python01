c9baf28531d4b959f0ad68ce322111a4bb0f3e35
pipelin56 <pipelin56@hotmail.com>
2015-03-12 23:02:26 +0100

    Adding exercise of 99BottlesOfBeer.py

---
+++ 99BottlesOfBeer.py
@@ -0,0 +1,6 @@
+botellas = 99
+print botellas, "bottles of beer on the wall,", botellas, "bottles of beer."
+while botellas > 0:
+    botellas=botellas-1
+    print "Take one down, pass it around,", botellas, "bottles of beer on the wall."
+    print botellas, "bottles of beer on the wall,", botellas, "bottles of beer."
